caa9b7a649c0a389dff73c55890a0581ed989b2b
Faizaldeen <faizaldeen098@gmail.com>
2024-09-19 13:58:21 +0530

    Add files via upload

--- js/project_BTM.js
+++ js/project_BTM.js
@@ -1,5 +1,19 @@
-document.addEventListener(`DOMContentLoaded`,function()
+document.addEventListener('DOMContentLoaded',function()
 {
-    const container=document.getElementById(``)
-}
-)
+    const container-fluid=document.getElementById('container-fluid');
+    function checkVisibility()
+    {
+        const rect=container-fluid.getBoundingClientRect();
+        const windowHeight=window.innerHeight||document.documentElement.clientHeight;
+        if(rect.top<=windowHeight&&rect.bottom>=0)
+        {
+            container.classList.add('visible');
+        }
+        else
+        {
+            container.classList.remove('visible');
+        }
+    }
+    window.addEventListener('scroll',checkVisibility);
+    checkVisibility();
+});
